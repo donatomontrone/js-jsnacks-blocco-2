b455af4c83018de11d372665f3646e809695282f
donatomontrone <donato.montrone@outlook.com>
2022-11-25 17:34:35 +0100

    add max number

--- snack-6/js/script.js
+++ snack-6/js/script.js
@@ -5,18 +5,22 @@
  */
 
 
-const numberArray = [1, 2, 3, 4, 5, 7, 9, 11, 13, 45, 67, 89];
+const numberArray = [1, 2, 3, 4, 32, 7, 9, 11, 13, 45, 78, 98];
 
 let sum =  0;
 let average = 0;
+let MaxNumber = numberArray[0];
 
 for (let index = 0; index < 10; index++) {
     sum += numberArray[index]
 
+    if (numberArray[index] > MaxNumber ) {
+        MaxNumber = numberArray[index];
+    }
 }
-
-console.log(sum)
 
 average = sum / 10;
 
-console.log(average)
+console.log(sum + 'somma');
+console.log(average + 'media');
+console.error(MaxNumber + 'numero massimo')
