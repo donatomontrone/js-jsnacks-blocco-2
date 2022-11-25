5282a509e504873d794d0585911d7a579efa81f1
donatomontrone <donato.montrone@outlook.com>
2022-11-25 16:48:01 +0100

    add average

--- snack-6/js/script.js
+++ snack-6/js/script.js
@@ -7,11 +7,16 @@
 
 const numberArray = [1, 2, 3, 4, 5, 7, 9, 11, 13, 45, 67, 89];
 
-let sum = 0;
+let sum =  0;
+let average = 0;
 
 for (let index = 0; index < 10; index++) {
     sum += numberArray[index]
-    
+
 }
 
 console.log(sum)
+
+average = sum / 10;
+
+console.log(average)
