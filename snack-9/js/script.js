76418c2a3f9d39ad4b0fafed06e2ad60b7d0cd70
donatomontrone <donato.montrone@outlook.com>
2022-11-28 12:51:26 +0100

    change for

--- snack-9/js/script.js
+++ snack-9/js/script.js
@@ -4,9 +4,7 @@
 
 const numbersInt = [3,7,5,34,65,87,23,567,6577,3243,654,7869,786,12,3454,6432532];
 let sum = 0;
-for (let index = 0; index < numbersInt.length; index++) {
-    if (numbersInt[index] % 2 == 1) {
+for (let index = 1; index < numbersInt.length; index+= 2) {
         sum += numbersInt[index];
-    }
 }
 console.log(sum);
