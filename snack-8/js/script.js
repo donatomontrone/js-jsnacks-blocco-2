09f2c84e5acdf0dcf44a1ea7d021bd7b3f79f8a9
donatomontrone <donato.montrone@outlook.com>
2022-11-28 12:42:13 +0100

    added sum odd

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -5,17 +5,18 @@
  *
  */
 
-const names = ['Michela', 'Giuseppina', 'Taylor', 'Donna', 'Marsha', 'Fabio', 'Roberto'];
-const lastnames = ['Cappelletti', 'Jager', 'Porcinelli' , 'Buffetti' , 'Forghieri', 'Papagni', 'Marazzini'];
-const guests = [];
-
-
-while (guests.length < 20) {
-    const randomNamesIndex = Math.floor(Math.random() * (names.length));
-    const randomLastnamesIndex  = Math.floor(Math.random() * (lastnames.length));
-
-    let guest = names[randomNamesIndex] + " " + lastnames[randomLastnamesIndex];
-
-    guests.push(guest);
-}
-console.log(guests);
+ const names = ['Michela', 'Giuseppina', 'Taylor', 'Donna', 'Marsha', 'Fabio', 'Roberto'];
+ const lastnames = ['Cappelletti', 'Jager', 'Porcinelli' , 'Buffetti' , 'Forghieri', 'Papagni', 'Marazzini'];
+ const guests = [];
+ 
+ 
+ while (guests.length < 20) {
+     const randomNamesIndex = Math.floor(Math.random() * (names.length));
+     const randomLastnamesIndex  = Math.floor(Math.random() * (lastnames.length));
+ 
+     let guest = names[randomNamesIndex] + " " + lastnames[randomLastnamesIndex];
+ 
+     guests.push(guest);
+ }
+ console.log(guests);
+ 
