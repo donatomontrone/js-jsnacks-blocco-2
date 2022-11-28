60affe7d27a6cfb9b509351b06e29694360aa89f
donatomontrone <donato.montrone@outlook.com>
2022-11-28 12:44:48 +0100

    added semicolon

--- snack-9/js/script.js
+++ snack-9/js/script.js
@@ -3,10 +3,10 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
 const numbersInt = [3,7,5,34,65,87,23,567,6577,3243,654,7869,786,12,3454,6432532];
-let sum = 0
+let sum = 0;
 for (let index = 0; index < numbersInt.length; index++) {
     if (numbersInt[index] % 2 == 1) {
-        sum += numbersInt[index]
+        sum += numbersInt[index];
     }
 }
-console.log(sum)
+console.log(sum);
